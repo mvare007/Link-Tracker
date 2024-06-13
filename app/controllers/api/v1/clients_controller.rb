34a3631a8a27803376c7e3169fc23a1b7990f18c
marcovarela <marcovarela@aquaporservicos.pt>
2024-06-13 20:37:09 +0100

    Annotate routes

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -1,24 +1,26 @@
 module Api
-	module V1
-		class ClientsController < ApplicationController
-			def create
-				@client = Client.new(client_params)
-				if @client.save
-					render :show, status: :created
-				else
-					render json: { errors: @client.errors }, status: :unprocessable_entity
-				end
-			end
+  module V1
+    class ClientsController < ApplicationController
+      # @route GET /api/v1/clients/:id (api_v1_client)
+      def show
+        @client = Client.find(params[:id])
+      end
 
-			def show
-				@client = Client.find(params[:id])
-			end
+      # @route POST /api/v1/clients (api_v1_clients)
+      def create
+        @client = Client.new(client_params)
+        if @client.save
+          render :show, status: :created
+        else
+          render json: { errors: @client.errors }, status: :unprocessable_entity
+        end
+      end
 
-			private
+      private
 
-			def client_params
-				params.require(:client).permit(:name, :store_url)
-			end
-		end
-	end
+      def client_params
+        params.require(:client).permit(:name, :store_url)
+      end
+    end
+  end
 end
