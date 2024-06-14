5ed8b28b096a3d54d290589eb4f05130d1053a12
marcovarela <marcovarela@aquaporservicos.pt>
2024-06-14 01:51:13 +0100

    Sanitize URL before redirecting in LinksController

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -6,7 +6,7 @@ class LinksController < ApplicationController
     visit = Visit.new(tracking_link: @tracking_link, ip_address: request.remote_ip, user_agent: request.user_agent)
 
     if visit.save
-      redirect_to @tracking_link.client_store_url, allow_other_host: true # TODO: Sanitize URL
+      redirect_to sanitized_url(@tracking_link.client_store_url), allow_other_host: true
       # render json { redirect_to: @tracking_link.client_store_url }, status: :ok
     else
       Rails.logger.error "Failed to save visit due to the following errors:\nErrors: #{visit.errors.full_messages.join("\n")}"
@@ -19,5 +19,11 @@ class LinksController < ApplicationController
   def set_tracking_link
     @tracking_link = TrackingLink.find_by(tracking_code: params[:tracking_code])
     @tracking_link.present? or return render json: { errors: 'Tracking Link not found' }, status: :unprocessable_entity
+  end
+
+  def sanitized_url(url)
+    uri = URI.parse(url)
+    uri.scheme = 'https' if uri.scheme.nil?
+    uri.to_s if %w[http https].include?(uri.scheme)
   end
 end
